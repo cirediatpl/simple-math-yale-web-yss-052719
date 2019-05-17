9d72745320d1c5d4e282605af2a86e29dcf6c8a9
colorful-architect-9266 <nam-joong.hwang@yale.edu>
2019-05-17 15:48:34 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -12,18 +12,31 @@ subtraction(10, 5)
 
 def division(num1, num2)
   num1 / num2
-  division(50, 2)
 end
+
+division(50, 2)
 
 def multiplication(num1, num2)
+  num1 * num2
 end
+
+multiplication(4, 30)
 
 def modulo(num1, num2)
+  num1 % num2
 end
+
+modulo(34, 5)
 
 def square_root(num)
+  Math.sqrt(num)
 end
 
+square_root(81)
+
 def order_of_operation(num1, num2, num3, num4)
+  num1 + ((num2 * num3) / num4)
   #Hint:  __ + (( __ * __ ) / __ )
 end
+
+order_of_operation(7, 43, 23, 83)
